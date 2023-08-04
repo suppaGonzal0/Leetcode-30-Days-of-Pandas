d175e7dbc8f970020b93a4ca5e7eaeba7da4939c
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2023-08-04 15:39:08 +0600

    Update 1757. Recyclable and Low Fat Products.java

--- Data Filtering/1757. Recyclable and Low Fat Products.java
+++ Data Filtering/1757. Recyclable and Low Fat Products.java
@@ -10,6 +10,8 @@
 Write a solution to find the ids of products that are both low fat and recyclable.
 Return the result table in any order.
 The result format is in the following example.
+
+https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=30-days-of-pandas&lang=pythondata
 """
   
 
