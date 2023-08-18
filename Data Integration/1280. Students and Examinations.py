1b0eb19cf8326356cecac87be644c6e6dce89ac8
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2023-08-18 12:42:28 +0600

    Create 1280. Students and Examinations.py

---
+++ Data Integration/1280. Students and Examinations.py
@@ -0,0 +1,73 @@
+"""
+Input: 
+Students table:
++------------+--------------+
+| student_id | student_name |
++------------+--------------+
+| 1          | Alice        |
+| 2          | Bob          |
+| 13         | John         |
+| 6          | Alex         |
++------------+--------------+
+Subjects table:
++--------------+
+| subject_name |
++--------------+
+| Math         |
+| Physics      |
+| Programming  |
++--------------+
+Examinations table:
++------------+--------------+
+| student_id | subject_name |
++------------+--------------+
+| 1          | Math         |
+| 1          | Physics      |
+| 1          | Programming  |
+| 2          | Programming  |
+| 1          | Physics      |
+| 1          | Math         |
+| 13         | Math         |
+| 13         | Programming  |
+| 13         | Physics      |
+| 2          | Math         |
+| 1          | Math         |
++------------+--------------+
+Output: 
++------------+--------------+--------------+----------------+
+| student_id | student_name | subject_name | attended_exams |
++------------+--------------+--------------+----------------+
+| 1          | Alice        | Math         | 3              |
+| 1          | Alice        | Physics      | 2              |
+| 1          | Alice        | Programming  | 1              |
+| 2          | Bob          | Math         | 1              |
+| 2          | Bob          | Physics      | 0              |
+| 2          | Bob          | Programming  | 1              |
+| 6          | Alex         | Math         | 0              |
+| 6          | Alex         | Physics      | 0              |
+| 6          | Alex         | Programming  | 0              |
+| 13         | John         | Math         | 1              |
+| 13         | John         | Physics      | 1              |
+| 13         | John         | Programming  | 1              |
++------------+--------------+--------------+----------------+
+"""
+
+import pandas as pd
+
+def students_and_examinations(students: pd.DataFrame, subjects: pd.DataFrame, examinations: pd.DataFrame) -> pd.DataFrame:
+    return pd.merge(
+        left=pd.merge(
+            students, subjects, how='cross',
+        ).sort_values(
+            by=['student_id', 'subject_name']
+        ),
+        right=examinations.groupby(
+            ['student_id', 'subject_name'],
+        ).agg(
+            attended_exams=('subject_name', 'count')
+        ).reset_index(),
+        how='left',
+        on=['student_id', 'subject_name'],
+    ).fillna(0)[
+        ['student_id', 'student_name', 'subject_name', 'attended_exams']
+    ]
